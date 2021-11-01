b1c6682198b2868df20c2a2c965fc03151babef8
aatharvauti <bm.aatharvauti@protonmail.ch>
2021-11-01 11:03:10 +0530

    String Buffer Fix Issue

--- college/StringBuffer.java
+++ college/StringBuffer.java
@@ -1,14 +1,18 @@
 import java.lang.String;
+
 class StringBuffer
 {
-    public static void main(String arg[])
+    public static void main(String args[])
     {
-        StringBuffer sb = new StringBuffer("This is my college");
+        java.lang.StringBuffer sb = new java.lang.StringBuffer("This is my college");
+
         System.out.println("The string sb is: " + sb);
         System.out.println("The length sb is: " + sb.length());
         System.out.println("The capacity sb is: " + sb.capacity());
         System.out.println("The character at an index of 5 is: " + sb.charAt(6));
+
         sb.setCharAt(3,'x');
+
         System.out.println("After setting char x at position 3: " + sb);
         System.out.println("After appending: " + sb.append(" in Mumbai"));
         System.out.println("After inserting: " + sb.insert(19, "SAKEC"));
