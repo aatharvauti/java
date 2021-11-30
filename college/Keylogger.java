25f13add316f7fc50d196d724c27c8d0c1513bda
aatharvauti <bm.aatharvauti@protonmail.ch>
2021-11-30 10:53:08 +0530

    Added Keylogger

---
+++ college/Keylogger.java
@@ -0,0 +1,29 @@
+import java.awt.*; 
+import java.awt.event.*; 
+import java.applet.*; 
+
+public class Keylogger extends Applet implements KeyListener 
+{ 
+    String msg="";
+
+    public void init() { 
+        addKeyListener(this); 
+    }
+
+    public void keyPressed(KeyEvent k) { 
+        showStatus("KEY IS PRESSED"); 
+    }
+
+    public void keyReleased(KeyEvent k) {
+        showStatus("KEY IS RELEASED");
+    }
+
+    public void keyTyped(KeyEvent k) {
+        msg = msg + k.getKeyChar();
+        repaint();
+    }
+
+    public void paint(Graphics g) { 
+        g.drawString(msg, 20, 40);
+    }
+}
