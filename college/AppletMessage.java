09da967d75bf557f64600d91ea164042d24fd80f
aatharvauti <bm.aatharvauti@protonmail.ch>
2021-11-30 10:39:09 +0530

    Added Applet Message

---
+++ college/AppletMessage.java
@@ -0,0 +1,13 @@
+import java.applet.*;
+import java.awt.*;
+
+public class AppletMessage extends Applet 
+{
+    public void paint(Graphics g)
+    {
+        String msg="Hello, World! I am Atharva. Welcome to my Applet.";
+        g.drawString(msg, 80, 160);
+    }    
+}
+
+
